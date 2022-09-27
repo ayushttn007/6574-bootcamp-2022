1a49e70cd02349acf7fb3726770d7efb8da3a6c9
AyushSharma <ayush.sharma2@tothenew.com>
2022-09-27 10:30:20 +0530

    Add java1_28sep Exercise Question3 solution

---
+++ Java1/Question3.java
@@ -0,0 +1,62 @@
+package java1_exercise28sep;
+
+
+import java.util.Scanner;
+
+public class exercise_28sep {
+
+   static void areaOfCircle(int radius){
+       // area of circle = 22 / 7 * radius * radius;
+
+       int result = (int) (Math.PI * radius * radius);
+       System.out.println("Area of Circle: " + result);
+
+   }
+
+   static void Exit(){
+       System.out.println("Exit");
+   }
+
+   static void circumOfCircle(int radius){
+       int result =  (int) (Math.PI * radius * 2);
+       System.out.println("Circumfrence of Circle:" + result);
+   }
+    public static void main(String[] args){
+       Scanner scn = new Scanner(System.in);
+
+       System.out.println("*******Menu*******");
+       /* 1. Calculate Area of Circle
+2. Calculate Circumference of a Circle
+3. Exit.
+       * */
+        System.out.println("1. Calculate Area of Circle");
+        System.out.println("2. Calculate Circumference of a Circle");
+        System.out.println("3. Exit");
+         int radius ;
+        int input = scn.nextInt();
+
+        switch (input){
+            case 1: {
+                System.out.print("Radius: ");
+                radius = scn.nextInt();
+                areaOfCircle(radius);
+            }
+            break;
+            case 2: {
+                System.out.print("Radius: ");
+                radius = scn.nextInt();
+                circumOfCircle(radius);
+            }
+            break;
+            case 3: {
+                Exit();
+            }
+            break;
+        }
+    }
+
+
+
+
+
+}
